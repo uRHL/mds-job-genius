2bf50f3321f07ca4ce30554424f14cc3dddc30d8
Pablo Gómez-Caldito <pgomezcaldito@idealista.com>
2021-03-23 19:24:40 +0100

    Job facts service interface and job facts class

--- app/src/main/java/com/canonicalexamples/jobgenius/model/JobFacts.kt
+++ app/src/main/java/com/canonicalexamples/jobgenius/model/JobFacts.kt
@@ -1,7 +1,15 @@
 package com.canonicalexamples.jobgenius.model
 
 data class JobFacts(
-    val id: Int = 0,
+    val id: String = "",
+    val type: String = "",
+    val url: String = "",
+    val created_at: String = "",
+    val company: String = "",
+    val company_url: String = "",
+    val location: String = "",
     val title: String = "",
-    val completed: Boolean = false
+    val description: String = "",
+    val how_to_apply: String = "",
+    val company_logo: String = ""
 )
--- app/src/main/java/com/canonicalexamples/jobgenius/model/JobFactsService.kt
+++ app/src/main/java/com/canonicalexamples/jobgenius/model/JobFactsService.kt
@@ -2,14 +2,21 @@ package com.canonicalexamples.jobgenius.model
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface JobFactsService {
     @GET("/positions.json")
-    fun getJobs(): Call<JobFacts>
+    fun getJobs(@Query("page") page: String): Call<JobFacts>
 
     @GET("/positions.json?location=Remote")
-    fun getRemoteJobs(): Call<JobFacts>
+    fun getRemoteJobs(@Query("page") page: String): Call<JobFacts>
+
+    @GET("/positions.json")
+    fun getJobsWithDescription(@Query("page") page: String, @Query("description") description: String): Call<JobFacts>
+
+    @GET("/positions.json?location=Remote")
+    fun getRemoteJobsWithDescription(@Query("page") page: String, @Query("description") description: String): Call<JobFacts>
 
 
 }
